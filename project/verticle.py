2cd6e672cdb658847fad511af6f5f6d758cd8637
Egor31415 <egorka.ku06@mail.ru>
2023-11-28 22:01:07 +0800

    Done

--- project/verticle.py
+++ project/verticle.py
@@ -1,2 +1,30 @@
+import math
+import matplotlib.pyplot as plt
 class verticle:
-    pass
+    def __init__(self, name, x=None, y=None):
+        self.name = name
+        self.x = x
+        self.y = y
+    def __str__(self):
+        if self.x is not None and self.y is not None:
+            return f"{self.name} ({self.x}, {self.y})"
+        else:
+            return self.name
+    def set_coordinates(self, x, y):
+        self.x = x
+        self.y = y
+    def plot_on_cartesian(self):
+        if self.x is None or self.y is None:
+            print("Координаты не заданы.")
+        else:
+            print(f"Точка {self.name} с координатами ({self.x}, {self.y}).")
+            plt.scatter(self.x, self.y, color='blue')
+            plt.text(self.x, self.y, self.name)
+            plt.xlabel("X")
+            plt.ylabel("Y")
+            plt.title("Точки на декартовой системе координат")
+            plt.grid(True)
+            plt.show()
+
+p1 = verticle("A", 3, 4)
+p1.plot_on_cartesian()
