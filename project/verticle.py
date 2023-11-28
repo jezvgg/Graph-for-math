95139bb1430d41e4c03328c4c92a386626f5cc3c
Egor31415 <egorka.ku06@mail.ru>
2023-11-28 22:41:05 +0800

    Done

--- project/verticle.py
+++ project/verticle.py
@@ -1,18 +1,18 @@
 import math
 import matplotlib.pyplot as plt
+
 class verticle:
     def __init__(self, name, x=None, y=None):
         self.name = name
         self.x = x
         self.y = y
+
     def __str__(self):
         if self.x is not None and self.y is not None:
             return f"{self.name} ({self.x}, {self.y})"
         else:
             return self.name
-    def set_coordinates(self, x, y):
-        self.x = x
-        self.y = y
+
     def plot_on_cartesian(self):
         if self.x is None or self.y is None:
             print("Координаты не заданы.")
@@ -24,7 +24,7 @@ class verticle:
             plt.ylabel("Y")
             plt.title("Точки на декартовой системе координат")
             plt.grid(True)
-            plt.show()
 
-p1 = verticle("A", 3, 4)
-p1.plot_on_cartesian()
+if __name__ == "__main__":
+    p1 = verticle("A", 3, 4)
+    p1.plot_on_cartesian()
